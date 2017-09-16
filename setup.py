fb6e90c27da821a2a9446543ef8f487bf3ee219e
Marco Rougeth <marco@rougeth.com>
2017-09-16 02:02:29 -0300

    v0.0.1-alpha.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='bottery',
-    version='0.0.1',
+    version='0.0.1-alpha.1',
     author='Marco Rougeth',
     author_email='marco@rougeth.com',
     packages=find_packages(),
