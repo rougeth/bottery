40a4f41d8a63381f4d9143bfb67f6ef8e6241474
Leticia Portella <leportella@gmail.com>
2017-09-23 16:38:02 -0300

    Added test for render and Message instance

---
+++ tests/test_message.py
@@ -0,0 +1,26 @@
+import os
+import tempfile
+from datetime import datetime
+from unittest import mock
+
+from bottery.message import Message, render
+
+
+@mock.patch('bottery.message.settings')
+def test_render(mocked_settings):
+    '''
+    Test if a message template is rendered with a
+    specified context.
+    '''
+    message = Message(123, 'Telegram', 'Bottery', 'text', 1506191745, 'raw')
+    with tempfile.TemporaryDirectory() as tempdir:
+        path = os.path.join(tempdir, 'templates')
+        os.mkdir(path)
+
+        template_path = os.path.join(path, 'template.md')
+        with open(template_path, 'w') as template:
+            template.write('{{ user }} {{ text }}!')
+
+        mocked_settings.TEMPLATES = [path]
+        rendered = render(message, 'template.md', {'text': 'rocks'})
+        assert rendered == 'Bottery rocks!'
