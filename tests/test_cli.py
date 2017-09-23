b9cd13a211896b629b4804d5f080b769af071778
Marco Rougeth <marco@rougeth.com>
2017-09-23 15:58:58 -0300

    Test debug_option decorator

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,8 +1,40 @@
+import logging
 import os
 
+import click
 from click.testing import CliRunner
 
-from bottery.cli import cli
+from bottery.cli import cli, debug_option
+
+
+def test_debug_flag_enabled():
+    logger = logging.getLogger('bottery')
+    logger.setLevel(logging.INFO)
+
+    @click.command()
+    @debug_option
+    def hello(debug):
+        pass
+
+    runner = CliRunner()
+    runner.invoke(hello, ['--debug'])
+
+    assert logger.level == logging.DEBUG
+
+
+def test_debug_flag_disabled():
+    logger = logging.getLogger('bottery')
+    logger.setLevel(logging.INFO)
+
+    @click.command()
+    @debug_option
+    def hello(debug):
+        pass
+
+    runner = CliRunner()
+    runner.invoke(hello)
+
+    assert logger.level == logging.INFO
 
 
 def test_startproject():
