73373b669a7940ae42de0f70c6b69f570963c4b1
Leticia Portella <leportella@gmail.com>
2017-10-09 15:29:54 -0300

    Added DefaultPattern class

--- bottery/conf/patterns.py
+++ bottery/conf/patterns.py
@@ -7,3 +7,11 @@ class Pattern:
         if message.text == self.pattern:
             return self.view
         return False
+
+
+class DefaultPattern:
+    def __init__(self, view):
+        self.view = view
+
+    def check(self, message):
+        return self.view
