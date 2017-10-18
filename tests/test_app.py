9779a6cd77b6125e2f065ae8cc54eebb70ca75be
= <james.g.bradshaw@gmail.com>
2017-10-17 20:54:48 -0500

    forgot to add test docs :-/

--- tests/test_app.py
+++ tests/test_app.py
@@ -76,7 +76,7 @@ def test_app_configure_with_tasks(mocked_engine):
 
 
 def test_app_configure_with_multiple_tasks(mocked_engine):
-    """App should have empty tasks if not defined  on engine"""
+    """App should have multiple tasks if defined  on engine"""
     async def fake_task(session):
         await asyncio.sleep(0)
 
