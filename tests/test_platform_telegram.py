003bd5bad3c7d0afb56b75b0d115fd81be6eb76e
Shuhrat Dehkanov <k@efir.uz>
2018-01-13 03:15:54 +0900

    TelegramEngine: add tests for get_chat_id()

--- tests/test_platform_telegram.py
+++ tests/test_platform_telegram.py
@@ -2,6 +2,7 @@ import aiohttp
 import pytest
 
 from bottery.platform.telegram.api import TelegramAPI
+from bottery.platform.telegram.engine import TelegramEngine
 
 
 def test_platform_telegram_api_non_existent_method():
@@ -15,3 +16,20 @@ async def test_platform_telegram_api_get_updates():
     api = TelegramAPI('token', aiohttp.ClientSession)
     with pytest.raises(TypeError):
         await api.get_updates()
+
+
+def test_platform_telegram_engine_get_chat_id_private():
+    user = type('TelegramUser', (), {'id': '123'})
+    chat = type('TelegramChat', (), {'type': 'private'})
+    message = type('Message', (), {'user': user, 'chat': chat})
+    engine = TelegramEngine
+
+    assert engine.get_chat_id(engine, message) == '123'
+
+
+def test_platform_telegram_engine_get_chat_id_group():
+    chat = type('TelegramChat', (), {'id': '456', 'type': 'group'})
+    message = type('Message', (), {'chat': chat})
+    engine = TelegramEngine
+
+    assert engine.get_chat_id(engine, message) == '456'
