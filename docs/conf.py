019ab06262187984721d236279a6a748335c95e8
Marco Rougeth <marco@rougeth.com>
2018-01-15 00:53:58 -0200

    Improve docs layout with custom fonts and colors

--- docs/conf.py
+++ docs/conf.py
@@ -96,6 +96,7 @@ html_theme_options = {
     "github_user": "rougeth",
     "github_repo": "bottery",
     "github_banner": True,
+    "github_button": False,
 }
 
 # Theme options are theme-specific and customize the look and feel of a theme
@@ -107,7 +108,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -183,3 +184,7 @@ texinfo_documents = [
      author, 'Bottery', 'One line description of project.',
      'Miscellaneous'),
 ]
+
+
+def setup(app):
+    app.add_stylesheet('css/custom.css')
--- docs/index.rst
+++ docs/index.rst
@@ -21,6 +21,6 @@ This part of the documentation, which is mostly prose, begins with some backgrou
 
    installation
    quickstart
-   views 
+   views
    patterns
    contributing
